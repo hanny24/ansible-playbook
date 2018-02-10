b27bb1c687ec65cbd7e9dbf94cdd76658becc5ef
Jan Strnad <strnad@avast.com>
2018-02-10 22:51:04 +0100

    Fixed fzf init

--- fish/functions/_run_fasd.fish
+++
@@ -1,3 +0,0 @@
-function _run_fasd --on-event fish_preexec
-	fasd --proc (fasd --sanitize $argv) > "/dev/null" 2>&1
-end
--- fish/functions/fzf_init.fish
+++ fish/functions/fzf_init.fish
@@ -138,6 +138,8 @@ function fzf_init
 
     echo $dir
   end
-
+  function _run_fasd --on-event fish_preexec
+    fasd --proc (fasd --sanitize $argv) > "/dev/null" 2>&1
+  end
 end
 
