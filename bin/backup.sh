ac94586312350b97629b5af8a4c655cdb17e5715
Jan Strnad <strnad@avast.com>
2018-02-10 22:50:50 +0100

    Backup to pcloud

--- bin/backup.sh
+++ bin/backup.sh
@@ -6,12 +6,12 @@ gui_warn() { zenity --warning "${ZENITY_COMMON[@]}" --text="$*" >/dev/null 2>&1;
 gui_error() { zenity --error "${ZENITY_COMMON[@]}" --text="$*" >/dev/null 2>&1; }
 trap 'echo $( date ) Backup interrupted >&2; exit 2' INT TERM
 
-
-# Setting this, so the repo does not need to be given on the commandline:
-export BORG_REPO=/mnt/strnad/borg
-
 # Setting this, so you won't be asked for your repository passphrase:
-export BORG_PASSPHRASE=$(python /home/hanny/.bin/pass.py get 'Borg backup')
+until BORG_PASSPHRASE=$(python /home/hanny/.bin/pass.py copy 'Borg backup'); do
+    info "Invalid password. Try again!"
+done
+export BORG_PASSPHRASE="${BORG_PASSPHRASE}"
+
 # or this to ask an external program to supply the passphrase:
 #export BORG_PASSCOMMAND="zenity --password ${ZENITY_COMMON[@]} 2>/dev/null"
 
@@ -30,64 +30,65 @@ info "Starting backup"
 # Backup the most important directories into an archive named after
 # the machine this script is currently running on:
 
-borg create                         \
-    --verbose                       \
-    --filter AME                    \
-    --list                          \
-    --stats                         \
-    --show-rc                       \
-    --compression lz4               \
-    --exclude-caches                \
-    --exclude '/home/*/.cache/*'    \
-    --exclude '/var/cache/*'        \
-    --exclude '/var/tmp/*'          \
-    --exclude '/home/hanny/Stažené' \
-    --exclude '/home/hanny/.cache' \
-    --exclude '/home/hanny/.gradle/caches' \
-    --exclude '/home/hanny/.m2/repository' \
-    --exclude '/home/hanny/.ivy2/cache' \
-    --exclude '/home/hanny/.coursier' \
-    --exclude '/home/hanny/.cargo' \
-    --exclude '/home/hanny/VirtualBox VMs' \
-    --exclude '/home/hanny/.IntelliJIdea2017.3/system' \
-    --exclude '/home/hanny/.gradle' \
-    --exclude '/home/hanny/.rustup' \
-    --exclude '/home/hanny/.local/share/Trash/' \
-    ::'{hostname}-{now}'            \
-    /home/hanny                     \
+run_backup() {
+    borg create                         \
+        --verbose                       \
+        --filter AME                    \
+        --list                          \
+        --stats                         \
+        --show-rc                       \
+        --compression lz4               \
+        --exclude-caches                \
+        --exclude '/home/*/.cache/*'    \
+        --exclude '/var/cache/*'        \
+        --exclude '/var/tmp/*'          \
+        --exclude '/home/hanny/.cache' \
+        --exclude '/home/hanny/.gradle/caches' \
+        --exclude '/home/hanny/.m2/repository' \
+        --exclude '/home/hanny/.ivy2/cache' \
+        --exclude '/home/hanny/.coursier' \
+        --exclude '/home/hanny/.cargo' \
+        --exclude '/home/hanny/VirtualBox VMs' \
+        --exclude '/home/hanny/.IntelliJIdea2017.3/system' \
+        --exclude '/home/hanny/.gradle' \
+        --exclude '/home/hanny/.rustup' \
+        --exclude '/home/hanny/.local/share/Trash/' \
+        --exclude '/home/hanny/pCloudDrive' \
+        --exclude '/home/hanny/.pcloud' \
+        ::'{hostname}-{now}'            \
+        /home/hanny                     \
 
-backup_exit=$?
+    backup_exit=$?
+    info "Pruning repository"
 
-info "Pruning repository"
+    borg prune                          \
+        --list                          \
+        --prefix '{hostname}-'          \
+        --show-rc                       \
+        --keep-daily    7               \
+        --keep-weekly   4               \
+        --keep-monthly  6               \
 
-# Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
-# archives of THIS machine. The '{hostname}-' prefix is very important to
-# limit prune's operation to this machine's archives and not apply to
-# other machines' archives also:
+    prune_exit=$?
 
-borg prune                          \
-    --list                          \
-    --prefix '{hostname}-'          \
-    --show-rc                       \
-    --keep-daily    7               \
-    --keep-weekly   4               \
-    --keep-monthly  6               \
+    # use highest exit code as global exit code
+    global_exit=$(( backup_exit > prune_exit ? backup_exit : prune_exit ))
 
-prune_exit=$?
+    if [ ${global_exit} -eq 1 ];
+    then
+        info "Backup and/or Prune finished with a warning"
+        gui_warn "Backup and/or Prune finished with a warning"
+    fi
 
-# use highest exit code as global exit code
-global_exit=$(( backup_exit > prune_exit ? backup_exit : prune_exit ))
+    if [ ${global_exit} -gt 1 ];
+    then
+        info "Backup and/or Prune finished with an error"
+        gui_error "Backup and/or Prune finished with an error"
+    fi
+}
 
-if [ ${global_exit} -eq 1 ];
-then
-    info "Backup and/or Prune finished with a warning"
-    gui_warn "Backup and/or Prune finished with a warning"
-fi
+export BORG_REPO=/mnt/strnad/borg
+#run_backup
 
-if [ ${global_exit} -gt 1 ];
-then
-    info "Backup and/or Prune finished with an error"
-    gui_error "Backup and/or Prune finished with an error"
-fi
-
-exit ${global_exit}
+export BORG_REPO=/home/hanny/pCloudDrive/Backup/Borg
+run_backup
